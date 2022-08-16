08e8fa047abf986993e5aab671533f2c4d4f30b7
joaquinpereyra <joaquin.pereyrar@correo.ucu.edu.uy>
2022-08-16 18:54:13 -0300

    Patrones estructurales - Patrón Adapter

---
+++ Preparación entrevistas/src/Patrones/PatronesEstructurales/Adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package Patrones.PatronesEstructurales.Adapter;
+
+public interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}
---
+++ Preparación entrevistas/src/Patrones/PatronesEstructurales/Adapter/VlcPlayer.java
@@ -0,0 +1,18 @@
+package Patrones.PatronesEstructurales.Adapter;
+
+public class VlcPlayer implements AdvancedMediaPlayer{
+
+    @Override
+    public void playVlc(String fileName) {
+        System.out.println("Playing vlc file. Name: " + fileName);
+        
+    }
+
+    @Override
+    public void playMp4(String fileName) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
+}
