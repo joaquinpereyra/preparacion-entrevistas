a8448c5c55aca7b23428cdcdfa25fe25408ab1fb
joaquinpereyra <joaquin.pereyrar@correo.ucu.edu.uy>
2022-08-18 18:08:27 -0300

    Ejercicio TwoSum
    Dado un array de enteros y un int target pasados por parámetros, devolver un array de dos posiciones de los enteros que sumados sean iguales al target

---
+++ Preparación entrevistas/src/Arrays/TwoSum.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+import java.util.HashMap;
+
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. */
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target)
+    {
+        HashMap<Integer, Integer> neededIndex = new HashMap<>();
+        int[] result = new int[2];
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(neededIndex.get(nums[i]) != null)
+            {
+                int index2 = neededIndex.get(nums[i]);
+                return int[index2,i];/* 
+                result[0] = neededIndex.get(nums[i]);
+                result[1] = i;
+                break;*/
+            }
+            else
+            {
+                int needed = target - nums[i];
+                neededIndex.put(needed, i);
+            }
+        }
+        return result;
+
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int[] result = twoSum(nums,9);
+        System.out.println(result[0]+"   "  + result[1]);
+    }
+}
